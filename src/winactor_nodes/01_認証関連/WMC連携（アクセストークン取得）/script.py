0de60553161a1b748a8146f0a32ac4980292d508
d-shiina <daiki.shiina@gmail.com>
2025-06-30 22:50:37 +0900

    コメントを修正し、WinActorへの結果返却の表現を統一

--- src/winactor_nodes/01_認証関連/WMC連携（アクセストークン取得）/script.py
+++ src/winactor_nodes/01_認証関連/WMC連携（アクセストークン取得）/script.py
@@ -8,6 +8,7 @@ sys.path.append("C:\\Users\\Public\\msys-winactor\\libs")
 from winactor_for_wmc.auth import get_token
 
 def main(**kwargs):
+    # run関数の呼び出し
     return get_token.run(**kwargs)
 
 
@@ -20,5 +21,5 @@ if __name__ == "__main__":
     # mainの呼び出し
     result = main(base_url=base_url, user_id=user_id, password=password)
 
-    # 結果をWinActorに返す
+    # 結果をWinActorへ返す
     winactor.set_variable($アクセストークン$, result.get("token", ""))   # pyright: ignore
