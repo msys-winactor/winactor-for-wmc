4dfe755e9a1632270a4b90f323bb46b034611cc3
suiijima <iijima@hardstyle.co.jp>
2025-07-10 13:13:47 +0900

    #19 シナリオ登録の実装

---
+++ src/winactor_for_wmc/scenarios/post_scenarios.py
@@ -0,0 +1,39 @@
+from winactor_for_wmc.common import get_departments
+from winactor_for_wmc.common.client import WMCApiClient
+
+
+def run(**kwargs):
+    scenarios_url = kwargs.get("scenarios_url")
+    token = kwargs.get("token")
+    scenario_data = kwargs.get("scenario_data") or {}
+
+    # 所属名が渡されていればID変換
+    department_name1 = kwargs.get("department_name1")
+    department_name2 = kwargs.get("department_name2")
+    department_name3 = kwargs.get("department_name3")
+
+    if department_name1 or department_name2 or department_name3:
+        # 部門一覧取得
+        departments_result = get_departments.get_departments(
+            base_url=scenarios_url, token=token
+        )
+        department1_id, department2_id, department3_id = (
+            get_departments.get_departments_ids_by_names(
+                departments_result,
+                department_name1=department_name1,
+                department_name2=department_name2,
+                department_name3=department_name3,
+            )
+        )
+        if department1_id is not None:
+            scenario_data["department1"] = department1_id
+        if department2_id is not None:
+            scenario_data["department2"] = department2_id
+        if department3_id is not None:
+            scenario_data["department3"] = department3_id
+
+    # シナリオ登録API呼び出し
+    client = WMCApiClient(scenarios_url, token)
+    endpoint = "/scenarios"
+    result = client.post(endpoint, data=scenario_data)
+    return result
