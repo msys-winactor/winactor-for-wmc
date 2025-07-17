24cb75f4ba22a98e86a28d095bb840e5d80712d5
suiijima <iijima@hardstyle.co.jp>
2025-07-17 14:06:33 +0900

    #11 所属情報エクスポートの修正。clientモジュールとの連携

--- src/winactor_nodes/02_エクスポート関連/WMC所属情報エクスポート/script.py
+++ src/winactor_nodes/02_エクスポート関連/WMC所属情報エクスポート/script.py
@@ -1,33 +1,32 @@
 import sys
 import os
-import getpass
 
 sys.path.append("C:\\msys-winactor")
 sys.path.append("C:\\Users\\Public\\msys-winactor\\libs")
 
-import export_csv
+from winactor_for_wmc.departments import get_departments_csv
 
-# 各種パラメータ（WinActorの変数展開を想定）
-BASE_URL = !BASE_URL!  # type: ignore
-TOKEN = !TOKEN!        # type: ignore
-CSV_SAVE_PATH = !CSV_SAVE_PATH!  # type: ignore
+def main(**kwargs):
+    return get_departments_csv.run(**kwargs)
 
-# BASE_URLの末尾に「/」がなければ足す
-if not BASE_URL.endswith("/"):
-    BASE_URL += "/"
-CSV_EXPORT_URL = BASE_URL + "departments/csv"
+if __name__ == "__main__":
+    BASE_URL = !WMC URL!  # type: ignore
+    TOKEN = !アクセストークン!  # type: ignore
+    CSV_SAVE_PATH = !CSVファイル名!  # type: ignore
+    ENCODING = !エンコーディング|MS932,UTF-8!  # type: ignore
 
-params = {}
 
-try:
-    # 保存先ディレクトリがなければ作成
+    params = {
+        "encoding": ENCODING,
+    }
+
+
     save_dir = os.path.dirname(CSV_SAVE_PATH)
     os.makedirs(save_dir, exist_ok=True)
 
-    # CSVファイルをダウンロード＆保存
-    export_csv.export_csv(
-        CSV_EXPORT_URL, TOKEN, params, save_path=CSV_SAVE_PATH
+    result = main(
+        base_url=BASE_URL,
+        token=TOKEN,
+        save_path=CSV_SAVE_PATH,
+        params=params,
     )
-
-except Exception as e:
-    raise winactor.WinActorError(1, f"所属情報エクスポートエラー\n{str(e)}")  # type: ignore
