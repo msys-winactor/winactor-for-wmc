983e5ed25e88f199895afcb4cb17d37dc8db2af9
suiijima <iijima@hardstyle.co.jp>
2025-09-10 12:49:29 +0900

    #133 所属情報インポートの実装

---
+++ src/winactor_for_wmc/departments/post_departments_csv.py
@@ -0,0 +1,19 @@
+from winactor_for_wmc.common.client import WMCApiClient
+
+
+def run(**kwargs):
+    base_url = kwargs.get("base_url")
+    token = kwargs.get("token")
+    csv_file_path = kwargs.get("csv_file_path")
+    import_data = kwargs.get("import_data") or {}
+
+    # APIクライアント初期化
+    client = WMCApiClient(base_url, token)
+    endpoint = "/departments/csv"
+
+    # CSVファイルアップロードAPI呼び出し
+    with open(csv_file_path, "rb") as f:
+        files = {"file": f}
+        response = client.post(endpoint, data=import_data, files=files)
+
+    return response
